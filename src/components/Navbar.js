6cfd2895c21746b2605e0f387346e10ded9f3ed5
zucchero2304 <remek.piwowarski@gmail.com>
2022-03-06 20:11:21 +0100

    Added Context API

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,14 @@ import './Navbar.css'
 
 import { Link } from 'react-router-dom'
 import SearchBar from './SearchBar'
+import { useTheme } from '../hooks/useTheme'
 
 export default function Navbar() {
+
+    const { color } = useTheme()
+
     return (
-        <div className='navbar'>
+        <div className='navbar' style={{ background: color }}>
             <nav>
                 <Link to="/" className='brand'>
                     <h1>Cooking Ninja</h1>
--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ThemeContext, ThemeProvider } from './context/ThemeContext'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode >,
   document.getElementById('root')
 );
